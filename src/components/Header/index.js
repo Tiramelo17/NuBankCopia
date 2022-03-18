7105eae228493ef7a81d653d321b87c3e488e9fb
Tiramelo17 <meloferreiragabriel17@gmail.com>
2022-03-18 16:04:23 -0300

    Primeiro Commit, alterações em layout de tela principal

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Top, Logo, Title  } from "./styles";
+import logo from '~/assets/Nubank_Logo.png';
+import {Icone} from 'react-native-vector-icons/MaterialIcons'
+
+export default function Header() {
+  return (
+
+     <Container>
+       <Top>
+        <Logo source={logo}/>
+        <Title>Gabriel Ferreira Melo</Title>
+       </Top>
+
+      </Container>
+
+    )
+}
